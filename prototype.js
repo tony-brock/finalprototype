1693183475478a930e245971b54a44fe58b89866
Tony Brock <tonybrock@Tonys-Air.Home>
2020-05-04 15:09:16 -0400

    needslabels

--- prototype.js
+++ prototype.js
@@ -3,35 +3,31 @@ var dur = 500;
 var RecalculateScales = function(money, lengths)
 {
 //make Incomes an array
-var getIncomes = function(entry)
+    var getIncomes = function(entry)
     {
         return entry.Income
     };
-var Incomes = money.map(getIncomes);
-console.log("Incomes", Incomes);
+    var Incomes = money.map(getIncomes);
+    console.log("Incomes", Incomes);
 //make scores an array   
-var getThree = function(entry)
+    var getThree = function(entry)
     {
         return entry.Big_Three
     };
-var big_Three = money.map(getThree);
-console.log("Big Three", big_Three);
+    var big_Three = money.map(getThree);
+    console.log("Big Three", big_Three);
 //make self rates an array
-var getFinance = function(entry)
+    var getFinance = function(entry)
     {
         return entry.Financial_Matters
     };
-var finance = money.map(getFinance);
-console.log("Financial Matters Scores", finance);
+    var finance = money.map(getFinance);
+    console.log("Financial Matters Scores", finance);
     
     var xBase = d3.scaleBand()
             .domain(Incomes)
             .range([0,lengths.graph.width])
             .paddingInner(.5);
-    
-    var xBars = d3.scaleBand()
-            .domain(big_Three,finance)
-            .range([0,(lengths.graph.width/8)])
     
     var y1 = d3.scaleLinear()
             .domain([0,1])
@@ -41,61 +37,88 @@ console.log("Financial Matters Scores", finance);
             .domain([1,7])
             .range([lengths.graph.height,0]);
     
-    return {xBase:xBase, xBars:xBars, y1:y1, y2:y2};
+    return {xBase:xBase, y1:y1, y2:y2};
 };
 
 var updateGraph = function(target,money,lengths, Incomes, big_Three, Finance)
 {
     var scales = RecalculateScales(money,lengths);
     var xBase = scales.xBase;
-    var xBars = scales.xBars;
     var y1 = scales.y1;
     var y2 = scales.y2;
     
-    updateAxes(target,xBase,xBars,y1,y2)
+    updateAxes(target,xBase,y1,y2)
     
-    var rects = d3.select(target)
+    var rectsThree = d3.select(target)
         .select(".graph")
+        .append("g")
+        .attr("id", "big_Three")
         .selectAll("rect")
         .data(money)
     //might need accessor
-    rects.enter()
+    rectsThree.enter()
         .append("rect")
     
-    rects.exit()
+    rectsThree.exit()
         .remove()
     
     d3.select(target)
-        .select(".graph")
+        .select(".graph #big_Three")
         .selectAll("rect")
         .transition()
         .duration(500)
-        .attr("x", xBars(big_Three))
-        .attr("y", y1(big_Three))
-        .attr("width", xBars.bandwidth)
+        .attr("x", function(entry)
+             {
+                return xBase(entry.Income)+6
+            })
+        .attr("y", function(entry)
+            {
+                return y1(entry.Big_Three)
+            })
+        .attr("width", 20)
         .attr("height", function(entry)
-        {
-            return lengths.graph.height - y1(entry.Big_Three);
-        })
+            {
+                return lengths.graph.height - y1(entry.Big_Three);
+            })
         .attr("rx", 2)
         .attr("ry", 2)
         .attr("fill", "green") 
     
-    d3.select(target)
+    rectsFin = d3.select(target)
         .select(".graph")
+        .append("g")
+        .attr("id", "financial")
+        .selectAll("rect")
+        .data(money)
+    //might need accessor
+    rectsFin.enter()
+        .append("rect")
+    
+    rectsFin.exit()
+        .remove()
+    
+    d3.select(target)
+        .select(".graph #financial")
         .selectAll("rect")
         .transition()
         .duration(500)
-        .attr("x", xBars(Finance))
-        .attr("y", y2(Finance))
-        .attr("width", xBars.bandwidth)
+        .attr("x", function(entry)
+                    {
+                        return xBase(entry.Income)+27
+                    })
+        .attr("y", function(entry)
+                    {
+                        return y2(entry.Financial_Matters)
+                    })
+        .attr("width", 20)
         .attr("height", function(entry)
-        {
-            return lengths.graph.height - y1(entry.Big_Three);
-        })
+                    {
+                        return lengths.graph.height - y2(entry.Financial_Matters);
+                    })
         .attr("rx", 2)
         .attr("ry", 2)
         .attr("fill", "orange") 
+//        .attr("tranform", "translate("100+")")
 };
 
 var createLabels = function(lengths,target)
@@ -110,6 +133,15 @@ var createLabels = function(lengths,target)
         .classed("title", true)
         .attr("text-anchor", "middle")
         .attr("transform", "rotate(90)")
+    
+    labels.append("g")
+        .attr("transform","translate(20,"+ 
+              (lengths.margins.top+(lengths.graph.height/2))+")")
+        .append("text")
+        .text("S")
+        .classed("label",true)
+        .attr("text-anchor","middle")
+        .attr("transform","rotate(90)")
 };
     
 var initAxes = function(lengths, target)
@@ -125,12 +157,14 @@ var initAxes = function(lengths, target)
     
     axes.append("g")
         .attr("id","leftAxis")
-        .attr("transform","translate("+(lengths.margins.left-5)+","
+        .attr("transform","translate("+(lengths.margins.left)+","
              +(lengths.margins.top)+")")
+        .attr("stroke", "green")
     
     axes.append("g")
         .attr("id", "rightAxis")
         .attr("transform", "translate("+(lengths.margins.left+lengths.graph.width)+","+(lengths.margins.top)+")")
+        .attr("stroke", "orange")
 };
 
 var updateAxes = function(target,xBase,y1,y2)
